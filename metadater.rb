32c1a2d0013debd40d9f3b74913c0cf40cf774f0
Misty De Meo <mistydemeo@gmail.com>
2011-07-12 12:03:56 -0500

    Placeholder, simple arg handling

--- metadater.rb
+++ metadater.rb
@@ -1,7 +1,26 @@
-require 'rubygems'
+require 'rubygems' # When running under 1.8. Who knows?
 
 require 'mediainfo'
-require 'exiftool'
+require 'mini_exiftool'
 require 'spreadsheet'
 
-[ 'mov', 'avi', 'mp4', 'mts' ]
+# Internal functions and classes
+
+require './lib/mediainfo.rb'
+require './lib/exif.rb'
+require './lib/client.rb'
+
+# These are the filetypes we consider to be videos
+# All others are not videos and we care nothing for them
+# Note that we will also scan files with no extensions.
+filetypes = [ 'mov', 'avi', 'mp4', 'mts' ]
+
+# Get base directory to scan
+# This directory and all its subdirectories will be scanned
+scandir = ARGV[0]
+
+case ARGV[0]
+  when nil
+    puts "usage: metadater <directory>"
+    puts "metadater will scan the specified directory and produce a YAML and XLS output at the end of the process."
+end
