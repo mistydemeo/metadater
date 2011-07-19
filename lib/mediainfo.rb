ea43239460c08138c114e46f77ead768d8e88bbe
Misty De Meo <mistydemeo@gmail.com>
2011-07-19 16:28:58 -0500

    Add software metadata

--- lib/mediainfo.rb
+++ lib/mediainfo.rb
@@ -16,7 +16,7 @@ class Video < String
     exif = MiniExiftool.new self
 
     @@general   = {
-      :path => self,
+#      :path => self,
       :container => info.general.format,
       :video_tracks => info.video.count,
       :audio_tracks => info.audio.count,
@@ -26,6 +26,9 @@ class Video < String
 
     # Most videos only contain one video track.
 
+    if info.video[0].nil?
+      @@video = nil
+    else
     @@video     = {
       :codec => info.video.format,
       :codec_profile => info.video.format_profile,
@@ -35,13 +38,17 @@ class Video < String
       # :standard => TODO,            Write code to handle this
       :aspect_ratio => info.video.display_aspect_ratio,
       :bitrate => info.video.bit_rate,
-      :bitrate_mode => info.video.bitrate_mode,
+      # :bitrate_mode => info.video.bitrate_mode,     Not working for now
       :colour_space => info.video[0]["color_space"],
       :colour_subsampling => info.video[0]["chroma_subsampling"]
     }
+    end
 
     # We only log metadata from the first audio track,
     # But we will say how many tracks there are.
+    if info.audio[0].nil?
+      @@audio = nil
+    else
     @@audio     = {
       :codec => info.audio[0].format,
       :codec_id => info.audio[0].codec_id,
@@ -50,26 +57,41 @@ class Video < String
       :bit_depth => info.audio[0]["bit_depth"],
       :bitrate => info.audio[0].bit_rate,
       :bitrate_mode => info.audio[0].bit_rate_mode
-    }
+    } unless info.audio[0].nil?
+    end
 
     @@software  = {
       :writing_library => info.general.writing_library,
       :software => exif.history_software_agent
     }
 
+    @@hardware = {
+      :manufacturer => exif.make,
+      :model => exif.model,
+      :aperture => exif.aperture,
+      :aperture_setting => exif.aperture_setting,
+      :shutter_speed => exif.shutter_speed,
+      :gain => exif.gain,
+      :exposure => exif.exposure_program,
+      :focus => exif.focus,
+      :image_stabilization => exif.image_stabilization
+    }
+
+
     # Push the hash onto the array as a new entry
     # Each new line in the array forms a new row in the spreadsheet,
     # and a new entry in the YAML
     # Remember, .push is destructive! Use with care.
 
-    array.push = {
-      :key => self,              # The file path is the key
+    array.push ({
+      :file => self.to_s,              # The file path is the key
     #  :md5 => TODO,          MD5 isn't implemented yet
       :general => @@general,
       :video => @@video,
       :audio => @@audio,
-      :software => @@software
-    }
+      :software => @@software,
+      :hardware => @@hardware
+    })
 
   end
 end
