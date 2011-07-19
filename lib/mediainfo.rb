ca734e43c9037c75dcb14e4a2c160b8550239cbd
Misty De Meo <mistydemeo@gmail.com>
2011-07-19 11:17:15 -0500

    Method to write the hash and array containing metadata. Still has todos.

--- lib/mediainfo.rb
+++ lib/mediainfo.rb
@@ -7,7 +7,53 @@ class Video < String
     # Call ExifTool to examine software metadata
     exif = MiniExiftool.new self
 
-    @@general   = { :path => f, :container => info.general.format, :duration => info.general.duration, :encoded_date => info.general.encoded_date }
-    @@video     = { :codec => info.video.format, :codec_profile => info.video.format_profile, :codec_id => info.video.codec_id, :width => info.video.width, :height => info.video.height, :standard => TODO, :aspect_ratio => info.video.display_aspect_ratio, :bitrate => info.video.bit_rate, :bitrate_mode => info.video.bitrate_mode, :colour_space => TODO, :colour_subsampling => TODO }
-    @@audio     = { :codec => info.audio.format, :codec_id => info.audio.codec_id, :sampling_rate => info.audio.sampling_rate, :channels => info.audio.channels, :bit_depth => TODO, :bit_rate => info.audio.bit_rate, :bit_rate_mode => info.audio.bit_rate_mode }
-    @@software  = { :writing_library => info.general.writing_library, :software => exif.history_software_agent } 
+    @@general   = {
+      :path => f,
+      :container => info.general.format,
+      :duration => info.general.duration,
+      :encoded_date => info.general.encoded_date
+    }
+
+    @@video     = {
+      :codec => info.video.format,
+      :codec_profile => info.video.format_profile,
+      :codec_id => info.video.codec_id,
+      :width => info.video.width,
+      :height => info.video.height,
+      # :standard => TODO,            Write code to handle this
+      :aspect_ratio => info.video.display_aspect_ratio,
+      :bitrate => info.video.bit_rate,
+      :bitrate_mode => info.video.bitrate_mode,
+      # :colour_space => TODO,        Can MediaInfo do this?
+      # :colour_subsampling => TODO   Ditto
+    }
+
+    @@audio     = {
+      :codec => info.audio.format,
+      :codec_id => info.audio.codec_id,
+      :sampling_rate => info.audio.sampling_rate,
+      :channels => info.audio.channels,
+      # :bit_depth => TODO,           Ditto
+      :bit_rate => info.audio.bit_rate,
+      :bit_rate_mode => info.audio.bit_rate_mode
+    }
+
+    @@software  = {
+      :writing_library => info.general.writing_library,
+      :software => exif.history_software_agent
+    }
+
+    # Push the hash onto the array as a new entry
+    # Each new line in the array forms a new row in the spreadsheet,
+    # and a new entry in the YAML
+    # Remember, .push is destructive! Use with care.
+
+    @@array.push = {
+      :key => f,
+    #  :md5 => TODO,          MD5 isn't implemented yet
+      :general => @@general,
+      :video => @@video,
+      :audio => @@audio,
+      :software => @@software
+    }
+
