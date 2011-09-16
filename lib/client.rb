8f6c5d12859d7d371f2e0ebd90e9a8e415d53729
Misty De Meo <mistydemeo@gmail.com>
2011-09-16 13:17:15 -0500

    Add JSON output option

--- lib/client.rb
+++ lib/client.rb
@@ -3,7 +3,7 @@
 
 class Path < String
 
-  def scan
+  def scan( filetypes )
     puts 'Scanning files...'
 
     files = Find.find( self.to_s ).collect do |path|
@@ -14,23 +14,25 @@ class Path < String
       end
 
       case File.extname( path ).downcase
-      when '', *@@filetypes
+      when '', *filetypes
         path
       else
         next
       end
     end.compact!
 
-    # Examine each file in our @@files array, then
+    # Examine each file in our files array, then
     # produce a new array with their metadata
-    videos = files.collect { |f| Video.new( f ).to_hash rescue nil }.compact!
+    videos = files.collect { |f| Video.new( f ).to_hash rescue nil }.compact
 
     # OK, we have our metadata in a pretty object now
-    # Write to YAML
+    # Write to YAML & JSON
 
-    File.open( self + '/video.yml', 'w' ).write videos.to_yaml
+    File.open( self + '/video.yml', 'w' ).write videos.to_yaml unless ARGV.include? '--no-yaml'
+    File.open( self + '/video.json', 'w' ).write JSON.pretty_generate videos if ARGV.include? '--json'
 
-    puts "YAML written to #{self}/video.yml"
+    puts "YAML written to #{self}/video.yml" unless ARGV.include? '--no-yaml'
+    puts "JSON written to #{self}/video.json" if ARGV.include? '--json'
   end
 
 end
--- metadater.rb
+++ metadater.rb
@@ -3,7 +3,8 @@ require 'rubygems'      # When running under 1.8. Who knows?
 require 'mediainfo'     # Capture technical metadata from video. Requires MediaInfo
 require 'mini_exiftool' # Capture software metadata from video. Requires ExifTool
 require 'find'          # Ruby's Find.find method
-require 'yaml'          # Adds read + write for YAML
+require 'yaml' unless ARGV.include? '--no-yaml'
+require 'json' if ARGV.include? '--json'
 require 'digest/md5' unless ARGV.include? '--no-hash'
 
 # Internal functions and classes
@@ -14,18 +15,20 @@ require File.dirname(__FILE__) + '/lib/client.rb'
 # These are the filetypes we consider to be videos
 # All others are not videos and we care nothing for them
 # Note that we will also scan files with no extensions.
-@@filetypes = [ '.mov', '.avi', '.mp4', '.m4v', '.flv', '.mpg', '.mpeg', 'm2v', '.mts', '.mkv', '.swf' ]
+filetypes = [ '.mov', '.avi', '.mp4', '.m4v', '.flv', '.mpg', '.mpeg', 'm2v', '.mts', '.mkv', '.swf' ]
 
 case ARGV[0]
   when nil
     puts "usage: metadater <directory>"
     puts
-    puts "metadater will scan the specified directory and produce YAML output at the end"
-    puts "of the process."
+    puts "metadater will scan the specified directory and produce YAML and/or JSON output"
+    puts "at the end of the process."
     puts
     puts "Options:"
     puts "    --no-hash     Do not calculate an MD5 hash for the scanned files"
     puts "    --full-hash   Hash the complete file. This may take a very long time"
+    puts "    --json        Write output as JSON"
+    puts "    --no-yaml     Do not write output to YAML"
 
     exit
   when String
@@ -34,4 +37,4 @@ case ARGV[0]
     scandir = Path.new( ARGV[0].chomp '/' )
 end
 
-scandir.scan
+scandir.scan( filetypes )
